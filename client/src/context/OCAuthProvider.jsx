615c3641199c233911a20d1a8b39d5a612939370
Dev Bulchandani <devbulchandani8@gmail.com>
2024-08-31 01:35:31 +0530

    Error Boundary

--- client/src/context/OCAuthProvider.jsx
+++ client/src/context/OCAuthProvider.jsx
@@ -3,7 +3,7 @@ import { OCConnect } from '@opencampus/ocid-connect-js';
 
 const OCAuthProvider = ({ children }) => {
     const opts = {
-        redirectUri: 'http://localhost:5173/redirect', // Ensure this matches your configuration
+        redirectUri: 'https://setu-1.onrender.com/redirect', // Ensure this matches your configuration
     };
 
     return (
