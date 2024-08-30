6ce2bb16f4e1022c1013dfbb16626bee7c458624
Dev Bulchandani <devbulchandani8@gmail.com>
2024-08-31 00:52:42 +0530

    Deploy

--- client/src/apiClient.js
+++ client/src/apiClient.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:4000/api';
+const API_BASE_URL = 'https://setu.onrender.com/api';
 
 // Authenticate User (Student)
 export const authenticateUser = async (openIdUsername, ethAddress) => {
--- client/src/pages/MainHome.jsx
+++ client/src/pages/MainHome.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const MainHome = () => {
     return (
-        <div className=""></div>
+        <div className="">
+            <img src='./src/assets/WhatsApp Image 2024-08-31 at 00.06.17.jpeg' />
+        </div>
     );
 };
 
